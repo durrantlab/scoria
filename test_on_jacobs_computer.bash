8224854626e58ec4583b21562952764fafdec09a
jdurrant <jacobdurrant@gmail.com>
2016-12-03 15:58:03 -0500

    Added file for testing on a Mac.

---
+++ test_on_jacobs_computer.bash
@@ -0,0 +1,11 @@
+# Mac OSX Sierra (10.12.1)
+# Python 2.7.12 |Anaconda 2.2.0 OR PyPy 1.8.0
+# NumPy 1.11.0
+# SciPy 0.16.0
+# MDAnalysis 0.15.0
+
+echo "TRYING WITH TRADITIONAL PYTHON"
+python unit_tests.py
+
+echo "TRYING WITH PYPY"
+pypy unit_tests.py
